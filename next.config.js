db17e3cc50374679a0a0dd74b16af512d77ada41
Jesse Smith <jessesmith@Jesses-MacBook-Pro.local>
2025-11-04 17:31:38 -0700

    prep for hosting

--- next.config.js
+++ next.config.js
@@ -1,1 +1,13 @@
-module.exports = { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // If your repository is NOT named 'username.github.io', uncomment and set the basePath:
+  // basePath: '/your-repo-name',
+  // trailingSlash: true,
+};
+
+module.exports = nextConfig;
